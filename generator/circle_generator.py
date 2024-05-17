2a7f0fc7db6c3533902f55cec711f2c4437e6efc
AB21074 <ab21074@shibaura-it.ac.jp>
2024-05-17 18:09:44 +0900

    Create circle_generator.py
    円の座標情報を生成するクラス

---
+++ generator/circle_generator.py
@@ -0,0 +1,27 @@
+from generator.param_generator import ParamGenerator
+import numpy as np
+
+class CircleGenerater(ParamGenerator):
+    """"円エンティティを生成するクラス."""
+    
+    def __init__(self, drawing_size: tuple[int] = (400, 280)):
+        """イニシャライザ."""
+        super().__init__(drawing_size)
+    
+    def gen_circles(self, size: int = 1):
+        """任意サイズの円の中心と半径を生成する."""
+        centers = self.gen_cord(size)
+        radii = np.zeros(size)
+        pad = np.zeros([size, 5])
+
+        for i, (x, y) in enumerate(centers):
+            max_radius_x = min(x, self.sizeX - x)
+            max_radius_y = min(y, self.sizeY - y)
+            max_radius = min(max_radius_x, max_radius_y)
+            radii[i] = self.rng.random() * max_radius
+        
+        radii = radii.reshape(size,1)    
+        circles = np.hstack([pad,centers,radii,pad])
+        return circles
+        
+     
